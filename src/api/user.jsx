b516efe985f0fa712ed52644acc15fd9f71dfa3b
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-09-08 14:33:26 +0500

    error fixed

--- src/api/user.jsx
+++ src/api/user.jsx
@@ -1,6 +1,15 @@
-import api from "./axios";
+import axios from "axios";
 
 export const fetchUserProfile = async () => {
-  const res = await api.get("/users/get-profile");
-  return res.data;
+  try {
+    const res = await axios.get(
+      `${import.meta.env.VITE_SNAPTIDE_URL}/users/get-profile`,
+      {
+        withCredentials: true,
+      }
+    );
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
 };
